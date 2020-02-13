037207578de89b3ab98ea3a8a84a27caa54a55d2
김정덕 <ijung1@naver.com>
2020-02-13 16:44:29 +0900

    [Add] Order,Alarm 주문, 푸시알람 버튼 컴포넌트 생성

---
+++ ssafy-frontend/src/components/alarm.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <v-btn dark color="teal lighten-3" @click="checkIt()">
+      <!-- 장바구니 담은게 있으면,, -->
+      <v-badge color="red" :content="quantity"></v-badge>
+      <!-- 장바구니 담은게 없으면,,, -->
+    </v-btn>
+    <p style="display:none">{{count}}</p>
+  </div>
+</template>
+
+<script>
+import store from "@/vuex/store.js";
+export default {
+  data() {
+    return {
+      quantity: 0
+    };
+  },
+  computed: {
+    count() {
+      return (this.quantity = store.state.order);
+    }
+  },
+  methods: {
+    checkIt() {
+      this.$store.commit("CLEAR_ORDER");
+      this.quantity = 0;
+    }
+  }
+};
+</script>
+
+<style>
+</style>
